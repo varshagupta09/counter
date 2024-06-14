4112ce9f6c9cc00d643b8367d4a6ba762d98be7c
Varsha Gupta <vrsa8896@gmail.com>
2024-06-14 12:33:28 +0530

    km

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let Counter = 5;
+
+  const [counter, SetCounter] = useState(Counter);
+
+
+const addValue = () => {
+  Counter = Counter+1;
+  console.log(Counter);
+  SetCounter(Counter)
+}
+
   return (
-  
+    <div className="App">
+     <h1>Welcome! </h1>
+     <h2>Counter Value : {Counter} </h2>
+
+     <button onClick={addValue}>Add Value</button>
+     <br />
+     <br />
+
+     <button>Remove Value</button>
+    </div>
   );
 }
 
